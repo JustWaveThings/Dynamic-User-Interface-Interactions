1aa7fc1023b446accc404bbbf030d41461e800c5
JustWaveThings <justwavethings@proton.me>
2023-03-07 14:30:55 -0500

    Putting this on ice - it is not what the task asked for

--- src/modules/component_helpers/progressively collapsing menu/menuItem.js
+++ src/modules/component_helpers/progressively collapsing menu/menuItem.js
@@ -1,9 +1,35 @@
-const MenuItem = (() => {
+/* 
+
+Putting this on ice - it is not what the task asked for.
+
+*/
+
+/* 
+- menuItemLevel is the level of the menu item in the menu hierarchy
+1 = top level menu item  ul.nav list - the anchor. Only one per menu.
+2 = li .nav . item  
+3 = ul .sub-nav 
+4 = li .sub-li 
+5 = span .sub-span  - where the menuItem text will be placed. 
+
+- menuItemText is the text that will be displayed in the menu item
+
+
+*/
+
+function CreateMenuItem() {
 	// private
 	let menuItem;
+	const createMenuItem = (menuItemLevel, menuItemText) => {
+		switch (menuItemLevel) {
+			case 1:
+				break;
 
-	const createMenuItem = (menuItemText) => {
-		menuItem = document.createElement('li');
+			default:
+				break;
+		}
+
+		menuItem = document.createElement('');
 		menuItem.classList.add('menu-item');
 		menuItem.textContent = menuItemText;
 		return menuItem;
@@ -13,4 +39,4 @@ const MenuItem = (() => {
 	return {
 		init: (menuItemText) => createMenuItem(menuItemText),
 	};
-})();
+}
