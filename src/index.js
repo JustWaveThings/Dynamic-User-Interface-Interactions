ba5201a08fff91c0918ee4673b2a8e07f2f3d22b
JustWaveThings <justwavethings@proton.me>
2023-03-11 13:39:36 -0500

    Add container for slider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './style.css';
 import toggle from './modules/component_helpers/progressively collapsing menu/toggleClickedElement';
-import createCats from './modules/component_helpers/image_slider/cats';
+import { createCats } from './modules/component_helpers/image_slider/cats';
+import sliderInterfaceContainer from './modules/component_helpers/image_slider/sliderInterfaceContainer';
 
 toggle.init('.nav.item');
 
 createCats();
+
+const main = document.querySelector('.main');
+main.appendChild(sliderInterfaceContainer());
