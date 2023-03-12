39495e9e5237b80d1514d7afa76b735efd4b6f10
JustWaveThings <justwavethings@proton.me>
2023-03-12 15:49:38 +0000

    Create image slider class to toggle visibility when active

--- src/modules/component_helpers/image_slider/createImage.js
+++ src/modules/component_helpers/image_slider/createImage.js
@@ -1,9 +1,10 @@
-function createImage(src, alt, width = '600', height = '400') {
+function createImage(src, alt,  width = '600', height = '400', ) {
 	const img = new Image();
 	img.src = src;
 	img.alt = alt;
 	img.width = width;
 	img.height = height;
+
 	return img;
 }
 
